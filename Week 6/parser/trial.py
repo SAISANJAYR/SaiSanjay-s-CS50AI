ac0655919f12e00eb3d343e29641737a7428b9b0
SaiSanjay R <166155275+SAISANJAYR@users.noreply.github.com>
2025-04-26 13:19:46 +0530

    added week 6 projects

---
+++ Week 6/parser/trial.py
@@ -0,0 +1,39 @@
+import nltk
+import sys
+
+TERMINALS = """
+Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
+Adv -> "down" | "here" | "never"
+Conj -> "and" | "until"
+Det -> "a" | "an" | "his" | "my" | "the"
+N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
+N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she"
+N -> "smile" | "thursday" | "walk" | "we" | "word"
+P -> "at" | "before" | "in" | "of" | "on" | "to"
+V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
+V -> "smiled" | "tell" | "were"
+"""
+
+NONTERMINALS = """
+S -> NP VP | NP VP Conj NP VP | NP VP Conj VP
+NP -> N | Det N | Det AP N | P NP | NP P NP
+VP -> V | Adv VP | V Adv | VP NP | V NP Adv
+AP -> Adj | AP Adj
+"""
+
+grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
+parser = nltk.ChartParser(grammar)
+
+s = input("Sentence: ")
+
+tokens = nltk.word_tokenize(s)
+final_tokens = []
+
+for token in tokens:
+    token = token.lower()
+    if any(char.isalpha() for char in token):
+        final_tokens.append(token)
+    else:
+        continue
+
+print(final_tokens)
